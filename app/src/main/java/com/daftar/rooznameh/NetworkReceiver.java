dfff0337df1c09e33052ae67786e42ed3f977dce
enaammahmoodi-ops <enaammahmoodi@gmail.com>
2026-06-18 01:03:26 +0330

    Update NetworkReceiver.java

--- app/src/main/java/com/daftar/rooznameh/NetworkReceiver.java
+++ app/src/main/java/com/daftar/rooznameh/NetworkReceiver.java
@@ -3,10 +3,31 @@ package com.daftar.rooznameh;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Handler;
+import android.os.Looper;
 
 public class NetworkReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        SmsReceiver.retryPendingSms(context);
+        if (isOnline(context)) {
+            new Handler(Looper.getMainLooper()).postDelayed(() -> {
+                SmsReceiver.retryPendingSms(context);
+            }, 3000);
+        }
+    }
+
+    private boolean isOnline(Context context) {
+        try {
+            ConnectivityManager cm =
+                    (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+            NetworkInfo info = cm.getActiveNetworkInfo();
+            return info != null && info.isConnected();
+
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
