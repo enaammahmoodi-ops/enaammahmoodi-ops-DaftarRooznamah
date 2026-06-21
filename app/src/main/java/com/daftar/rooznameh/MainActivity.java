f89da8d090978624af775732200718a5ef7c5bde
enaammahmoodi-ops <enaammahmoodi@gmail.com>
2026-06-21 16:19:09 +0330

    Update MainActivity.java

--- app/src/main/java/com/daftar/rooznameh/MainActivity.java
+++ app/src/main/java/com/daftar/rooznameh/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends Activity {
     private Runnable syncRunnable = new Runnable() {
         @Override
         public void run() {
-            SmsReceiver.scanInboxBankSms(MainActivity.this, 300);
+            SmsReceiver.scanInboxBankSms(MainActivity.this, 500);
             SmsReceiver.retryPendingSms(MainActivity.this);
             syncHandler.postDelayed(this, 10000);
         }
@@ -49,7 +49,7 @@ public class MainActivity extends Activity {
 
         requestSmsPermission();
 
-        SmsReceiver.scanInboxBankSms(this, 300);
+        SmsReceiver.scanInboxBankSms(this, 500);
         SmsReceiver.retryPendingSms(this);
 
         syncHandler.postDelayed(syncRunnable, 10000);
@@ -74,8 +74,7 @@ public class MainActivity extends Activity {
     @Override
     public void onResume() {
         super.onResume();
-
-        SmsReceiver.scanInboxBankSms(this, 300);
+        SmsReceiver.scanInboxBankSms(this, 500);
         SmsReceiver.retryPendingSms(this);
     }
 
@@ -87,10 +86,7 @@ public class MainActivity extends Activity {
 
     @Override
     public void onBackPressed() {
-        if (webView != null && webView.canGoBack()) {
-            webView.goBack();
-        } else {
-            super.onBackPressed();
-        }
+        if (webView != null && webView.canGoBack()) webView.goBack();
+        else super.onBackPressed();
     }
 }
